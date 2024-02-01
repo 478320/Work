d359ea6a7a1290bd2f7a62efa7a5ae09e675a0ff
Yusiheng <2874602095@qq.com>
2024-02-01 17:31:57 +0800

    add: encryption processing

--- Warmup/todolist/src/test/java/com/huayu/TodolistApplicationTests.java
+++ Warmup/todolist/src/test/java/com/huayu/TodolistApplicationTests.java
@@ -1,13 +1,21 @@
 package com.huayu;
 
+import com.huayu.utils.JasyptUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.util.text.BasicTextEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class TodolistApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testEncrypt() {
+
+
     }
 
 }
